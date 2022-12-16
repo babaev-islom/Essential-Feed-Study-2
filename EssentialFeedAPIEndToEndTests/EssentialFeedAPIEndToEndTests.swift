f17792d6a53a096c4885313687c184cfa838cbac
babaev-islom <babaev.iislom@gmail.com>
2022-12-16 10:26:01 +0500

    Init end-to-end test target

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Islom on 16/12/22.
+//
+
+import XCTest
+
+final class EssentialFeedAPIEndToEndTests: XCTestCase {
+
+
+}
